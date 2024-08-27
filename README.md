ef8327d49c64fa671af90d7f14662d66f83d7aaf
Max Smith <maxsmith@Maxs-MacBook-Pro.local>
2024-08-27 14:08:45 -0700

    add 5 parts and 5 products to database

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ Changes Implemented:
         - AboutScreenController.java: Create file and contents
         - aboutScreen.html: Create file and contents
         - mainscreen.html Line 20: Add About Us button to redirect to About Us page
+    #Part E: Add a sample inventory appropriate for your chosen store to the application. You should have five parts and five products in your sample inventory and should not overwrite existing data in the database.
+        - BootStrapData.java Line 4: add import for inhousePart
+        - BootStrapData.java Line 8: add import for InhousePartRepository
+        - BootStrapData.java Line 34: add private final for InhousePartRepository
+        - BootStrapData.java Line 40: add alias for inhousePartRepository
+        - BootStrapData.java Line 46 - 80: add if statement to load outsourced parts if the count of the outsourced part repository is 0. This loads in pickups, tuners, and bridge outsourced parts.
+        - BootStrapData.java Line 83 - 96: add if statement to load inhouse parts if the count in the inhouse part repository is 0. This loads in guitar necks and guitar bodies.
+        - BootStrapData.java Line 99 - 110: Add if statement to load products into the product repository if the current repository count is 0. This loads in my 5 different guitar products (double cut, single cut, flying v, explorer, and headless).
+        
--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,9 +1,11 @@
 package com.example.demo.bootstrap;
 
 import com.example.demo.domain.OutsourcedPart;
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
 import com.example.demo.repositories.OutsourcedPartRepository;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
 import com.example.demo.service.OutsourcedPartService;
@@ -29,43 +31,83 @@ public class BootStrapData implements CommandLineRunner {
     private final ProductRepository productRepository;
 
     private final OutsourcedPartRepository outsourcedPartRepository;
+    private final InhousePartRepository inhousePartRepository;
 
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
+    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository, InhousePartRepository inhousePartRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
         this.outsourcedPartRepository=outsourcedPartRepository;
+        this.inhousePartRepository=inhousePartRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+        if(outsourcedPartRepository.count() == 0) {
+            OutsourcedPart pickups= new OutsourcedPart();
+            pickups.setCompanyName("Bareknuckle Pickups");
+            pickups.setName("Aftermath Pickups 6 String Set");
+            pickups.setInv(5);
+            pickups.setPrice(200.0);
+            pickups.setId(100L);
+            outsourcedPartRepository.save(pickups);
+            // OutsourcedPart thePart=null;
+            // List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            // for(OutsourcedPart part:outsourcedParts){
+            //     if(part.getName().equals("Aftermath Pickups 6 String Set"))thePart=part;
+            // }
+            // System.out.println(thePart.getCompanyName());
+
+            OutsourcedPart tuners= new OutsourcedPart();
+            tuners.setCompanyName("Hipshot");
+            tuners.setName("Hipshot Open Gear 6 String Set");
+            tuners.setInv(5);
+            tuners.setPrice(80.0);
+            tuners.setId(101L);
+            outsourcedPartRepository.save(tuners);
+
+            OutsourcedPart bridge= new OutsourcedPart();
+            bridge.setCompanyName("Hipshot");
+            bridge.setName("Hipshot Bridge 6 String");
+            bridge.setInv(5);
+            bridge.setPrice(200.0);
+            bridge.setId(102L);
+            outsourcedPartRepository.save(bridge);
+
+            // List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            // for(OutsourcedPart part:outsourcedParts){
+            //     System.out.println(part.getName()+" "+part.getCompanyName());
+            // }
         }
 
-        System.out.println(thePart.getCompanyName());
-        */
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            System.out.println(part.getName()+" "+part.getCompanyName());
-        }
+        if(inhousePartRepository.count() == 0) {
+            InhousePart guitarBody = new InhousePart();
+            guitarBody.setName("Standard Guitar Body");
+            guitarBody.setInv(5);
+            guitarBody.setPrice(500.0);
+            guitarBody.setId(103L);
+            inhousePartRepository.save(guitarBody);
 
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
+            InhousePart guitarNeck = new InhousePart();
+            guitarNeck.setName("Standard Guitar Neck");
+            guitarNeck.setInv(5);
+            guitarNeck.setPrice(300.0);
+            guitarNeck.setId(104L);
+            inhousePartRepository.save(guitarNeck);
+        }
+        
+        if(productRepository.count() == 0) {
+            Product doubleCut= new Product("Guitar - Double Cut",1500.0,15);
+            Product singleCut= new Product("Guitar - Single Cut",1500.0,15);
+            Product flyingV= new Product("Guitar - Flying V",1500.0,15);
+            Product explorer= new Product("Guitar - Explorer",1500.0,15);
+            Product headless= new Product("Guitar - Headless",1500.0,15);
+            productRepository.save(doubleCut);
+            productRepository.save(singleCut);
+            productRepository.save(flyingV);
+            productRepository.save(explorer);
+            productRepository.save(headless);
+        }
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
